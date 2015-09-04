c9f39e74b22f83560a7e28dfc989f78d68741736
Roelof Reitsma <r.reitsma@coolblue.nl>
2015-09-04 17:20:15 +0200

    First commit of the Ansible playbook for installing the MacBook

---
+++ install.sh
@@ -0,0 +1,7 @@
+# Install Homebrew from source
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install pip (python package manager)
+sudo easy_install pip
+# Use pip to provide latest available ansible package
+sudo pip install ansible
